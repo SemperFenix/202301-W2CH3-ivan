8d11d0dbbb0aea365ec259f69749ac963f44e91d
SemperFenix <dureivan@gmail.com>
2023-01-25 19:31:48 +0100

    Add test for arrayLength

---
+++ arrayMethods.test.js
@@ -0,0 +1,12 @@
+import * as arr from "./arrayMethods";
+
+describe("Given a function", () => {
+  describe("When array has 3 items", () => {
+    test("Then it should return the number of items", () => {
+      const array = ["a", "b", "c"];
+      const r = arr.arrayLength(array);
+      const result = 3;
+      expect(r).toBe(result);
+    });
+  });
+});
