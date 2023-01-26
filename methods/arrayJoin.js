707ce986a6d10e55f573f1ed42ecac8d6d052ccc
SemperFenix <dureivan@gmail.com>
2023-01-26 21:45:15 +0100

    Add arrayJoin function

--- methods/arrayJoin.js
+++ methods/arrayJoin.js
@@ -1,1 +1,23 @@
-export const arrayJoin = (array, element) => {};
+import { arrayLength } from "./arrayLength";
+
+export const arrayJoin = (array, element) => {
+  let result = "";
+  let i = 0;
+  if (element) {
+    while (i < arrayLength(array) - 1) {
+      result = result + array[i] + element;
+      i++;
+    }
+
+    return result + array[i];
+  }
+
+  while (i < arrayLength(array) - 1) {
+    result = result + array[i] + ",";
+    i++;
+  }
+
+  result += array[i];
+
+  return result;
+};
